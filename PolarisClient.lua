e4c915ca3ed36564ba800790382096603cf19361
Brandon Henness <Henness0666@gmail.com>
2016-12-23 21:21:43 -0800

    Adding some file structure.
    Added some main files, and added the modular addons folder.

---
+++ PolarisClient.lua
@@ -0,0 +1,36 @@
+--[[
+	|-------------------------------------------------------------------|
+
+	This Source Code Form is subject to the terms of the Mozilla Public
+	License, v. 2.0. If a copy of the MPL was not distributed with this
+	file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+	|-------------------------------------------------------------------|
+		Program:		Polaris
+		Class:          PolarisClient.lua
+		Description:    <Description>
+		Author:         Henness                   Date: 12/21/16 23:18
+		Notes:          <Notes>
+	|-------------------------------------------------------------------|
+]]--
+
+local pInfo = {} -- Program info to be called within the file.
+pInfo = {
+	programName = "Polaris" 			-- Program Name
+	className = "PolarisClient" 		-- File Name
+	author = "Henness" 					-- Author
+	version = 1.0 						-- Version
+}
+
+-- Functions
+	
+	-- Request update program function.
+	
+	-- Connect to login server function.
+	
+	-- Send login info funtion.
+		-- Hash password without storing.
+		-- Encrypt Username and Password for network communication.
+		-- Pass encrypted data to server.
+		
+	-- Draw GUI function.
